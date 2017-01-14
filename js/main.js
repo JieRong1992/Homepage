b72d1eb9b0661c17d74c7fff6f2e77234cdfbf38
jierong0512@gmail.com <jierong0512@gmail.com>
2017-01-14 16:54:19 -0500

    nav

--- js/main.js
+++ js/main.js
@@ -18,4 +18,36 @@ $(document).ready(function(){
         left: "999px",
     }, 500, "swing");
 });
+
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>50){
+            $("#sticker-header").fadeIn(500);
+        }else{
+            $("#sticker-header").fadeOut(500);
+        }
+    });
+
+    $(window).scroll(function(){
+        var top = $(document).scrollTop();
+        var nav = $(".nav");
+        var items =$("#content").find(".item");
+        var currentId="";
+
+        items.each(function(){
+            var current = $(this);
+            var itemTop= current.offset().top;
+            if(top>itemTop-200){
+                currentId = "#" + current.attr("id");
+            }else{
+                return false;
+            }
+        });
+
+        var currentLink = nav.find(".current");
+        if(currentId && currentLink.attr("href")!=currentId){
+            currentLink.removeClass("current");
+            nav.find("[href='"+currentId+"']").addClass("current");
+        }
+    });
 });
